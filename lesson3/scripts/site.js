e5f2ac0a2a9be7a0917a3e8f049ab0f5d8247cf1
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-01-28 21:39:45 -0700

    chamber

--- lesson3/scripts/site.js
+++ lesson3/scripts/site.js
@@ -4,3 +4,11 @@ document.querySelector("#currentyear").textContent = today.getFullYear();
 
 // sets last modified date on the home page
 document.querySelector("#lastmodified").textContent = document.lastModified;
+
+function toggleMenu(){
+    document.querySelector("#navbar").classList.toggle('menu-active')
+    document.querySelector("#menu-close").classList.toggle('menu-active')
+    document.querySelector("#menu-open").classList.toggle('menu-active')
+}
+
+document.querySelector("#hamburger-menu").addEventListener('click',toggleMenu);
