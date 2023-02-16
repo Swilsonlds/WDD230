1e50f6a1a6d1124eb91a907f0e32da022d6a5192
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-02-15 20:24:55 -0700

    added meeting banner

--- chamber/scripts/header.js
+++ chamber/scripts/header.js
@@ -1,6 +1,22 @@
+// Day of the week
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-
 document.querySelector(".date p").textContent = fulldate;
+
+// Meeting announcement to appear on Mondays and Tuesdays
+const d = new Date();
+let day = d.getDay();
+
+let meeting = document.querySelector("#meeting");
+
+if (day != 1 && day !=2){
+	meeting.style.display = "none";
+}
+
+else{
+	meeting.style.display = "block";
+}
+
+
