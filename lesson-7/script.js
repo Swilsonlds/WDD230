d98a4b4d19e7aad61375e58357b731e866c84969
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-02-17 00:25:58 -0700

    done I think?

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -1,30 +1,27 @@
-const images = document.querySelectorAll("[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -500px 0px"
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target)
-        }
-    })
-}, imgOptions);
+const callback = (items, observer) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+};
 
-images.forEach(image => {
-    imgObserver.observe(image);
-})
+let options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
+});
