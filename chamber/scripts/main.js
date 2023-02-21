4f20c074b3af3511ada077be0f82c7151931341e
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-02-20 21:02:33 -0700

    week 6 stuff

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -6,3 +6,22 @@ function toggleMenu() {
 }
 
 document.querySelector("#hamburger-button").addEventListener('click', toggleMenu);
+
+// weather
+function setWindChill(windspeed, temp){        
+    tempSpan = document.querySelector("#temperature");
+    windSpeedSpan = document.querySelector("#windspeed");
+    windChillSpan = document.querySelector("#windchill");
+
+    let windchill = 'N/A';
+    if (windspeed >= 3.0 && temp <= 50){
+        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+        windchill = `${chill}`;
+    }
+
+    tempSpan.textContent = temp;
+    windSpeedSpan.textContent = windspeed;
+    windChillSpan.innerHTML = windchill;
+}
+
+setWindChill(4, 33);
