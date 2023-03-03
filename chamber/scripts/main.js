1bbbb8647e3cf2408518473684344e1b86089800
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2023-03-02 20:02:48 -0700

    days since last visit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -27,3 +27,16 @@ function setWindChill(windspeed, temp){
 setWindChill(4, 33);
 
 // Days since last visit
+const MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+let lastVisitString = localStorage.getItem("lastVisit");
+let visitspan = document.querySelector('#days-since-visit');
+
+if (lastVisitString==null){        
+    visitspan.textContent = '0';
+}
+else{
+    lastVisitDate=new Date(lastVisitString);
+    daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY);
+    visitspan.textContent = daysSinceLastVisit;
+}
+localStorage.setItem("lastVisit", today.toLocaleDateString());
